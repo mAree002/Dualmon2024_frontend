1f6676e9dfe9cbf87367fbdc3abc7ffafcf46d69
teodora.jelovac <teodorajelovac27@gmail.com>
2024-06-06 16:11:43 +0200

    adds router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
+import React from "react";
 import './App.css'
-import Button from './components/Button'
-import NavigationHeader from './components/navigationHeader';
-import x from './components/navigation.module.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Navigation from './components/Navigation'
+import Home from './components/Home';
+import About from './components/Aboout';
+import Contacts from './components/Contacts';
+// import OtherPage from './components/OtherPage'
 
 function App() {
-
   return (
-    <div>
-        <Button title="Submit" variant="secondary"/>
-        <br/>
-        <Button title="Choose File" variant="primary"/>
-        <br/>
-        <NavigationHeader className={x.header} />
-
-    </div>
-  )
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        {/* <Route path="/other-page" element={<OtherPage/>}/> */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
-
+export default App;
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import styles from './navigation.module.css'
+import logo from './logo.png';
+
+function Navigation(){
+  return (
+    <div>
+    <nav className={styles.nav}>
+      <Link to="/"> Home </Link> 
+      <Link to="/about"> About </Link>
+      <Link to="/contacts"> Contacts </Link>
+    </nav>
+    {/* <img src={logo} alt="Logo" className={styles.logo} /> */}
+    {/* <button>
+    <Link to="/other-page">Go to Other Page</Link>
+    </button> */}
+    </div>
+  );
+};
+
+export default Navigation;
