d44d993b03c160604089b39aa1642aa7cb6afb26
teodora.jelovac <teodorajelovac27@gmail.com>
2024-06-10 12:54:57 +0200

    pirice range component

---
+++ src/components/PriceRange.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import Slider from 'react-slider';
+import styles from './pricerange.module.css'; 
+
+function PriceRange() {
+    const MIN = 10;
+    const MAX = 12000;
+    const [values, setValues] = useState([MIN, MAX]);
+    console.log('values:', values);
+    return (
+      <div className={styles.app}>
+        <div className={styles.box}> 
+          <div className={styles.span1}> 
+           <span className={styles.span2}>Price<br/></span>
+           <hr></hr>
+           <div className={styles.span2}>
+           <span >MIN </span><span className={styles['values-container']}>{values[0]}</span> - <span className={styles.span1}>MAX</span> <span className={styles['values-container']}>{values[1]}</span>
+          </div>
+          </div>
+          <Slider
+            className={styles.slider} 
+            thumbClassName={styles.thumb} 
+            trackClassName='track'
+            onChange={setValues} 
+            value={values}
+            min={MIN}
+            max={MAX}
+          />
+        </div>
+      </div>
+    );
+}
+  
+export default PriceRange;
